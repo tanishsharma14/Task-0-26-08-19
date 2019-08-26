2000e7c51bfb9b21ae6d60a6929fbe4568c2dcd8
SSI STUDENT <SSI STUDENT@HP09-PC>
2019-08-26 16:26:12 +0530

    First commit

---
+++ src/com/ssi/First.java
@@ -0,0 +1,14 @@
+package com.ssi;
+
+import com.ssi.*;
+
+public class First
+{
+	public static void main(String args[])
+	{
+		Circle c=new Circle();
+		Cylinder cy=new Cylinder();
+		
+		System.out.println(c.circleArea(10));
+	}
+}
